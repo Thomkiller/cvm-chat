ec5f4e6a488b9b1c98e7b37ade8a087d9e82522e
Thomas Pelletier <thomaspelletier@hotmail.ca>
2022-08-16 15:14:55 -0400

    animation index

--- client/src/page-index.js
+++ client/src/page-index.js
@@ -1,5 +1,10 @@
-import {signin} from './chat-api';
-
+import {
+    signin
+} from './chat-api';
+let spritelist = [];
+let spritelistheli = [];
+let spritelisttruck = [];
+let clicks = 0
 console.log("lol")
 
 window.addEventListener("load", () => {
@@ -7,25 +12,137 @@ window.addEventListener("load", () => {
         return signin(this);
     }
 
-   
-
 });
-// let Deadpool = document.getElementById("deadpool")
-// console.log(Deadpool)
-// Deadpool.addEventListener("click", () => {
-//     let node = document.createElement("div")
-//     let parentNode = document.querySelector("#background")
-//     node.setAttribute("id", "msg")
-//     node.innerText = "Welcome to Deadpool Chat"
-//     parentNode.append(node);
-//     })
 
-const fire = () => {
-    let node1 = document.createElement("div")
-    node1.id.add("fire")
-    let parentNode = document.querySelector("#background")
-    parentNode.append(node1)
+window.addEventListener("click", () => {
+    let sprite = new Fire("fire");
+    spritelist.push(sprite)
+    spritelist[clicks].fire();
+    clicks++;
+
+    if (clicks == 5) {
+        let helico = new Helicopter("helico");
+        spritelistheli.push(helico)
+        spritelistheli[0].helicopter()
+        bougerHeli();
+    }
+
+    if(clicks == 2){
+        let truck = new Firetruck("firetruck")
+        spritelisttruck.push(truck)
+        spritelisttruck[0].fireTruck();
+        bougerTruck();
+        
+
+    }
+
+
+})
+
+const bougerHeli = () => {
+    spritelistheli[0].move();
+    window.requestAnimationFrame(bougerHeli);
+}
+
+const bougerTruck = () => {
+    spritelisttruck[0].moveTruck()
+    window.requestAnimationFrame(bougerTruck)
+
 }
 
 
 
+
+class Fire {
+
+    constructor(id) {
+        this.node = document.querySelector("#" + id);
+        this.height = ((Math.random() * 250) + 600);
+        this.distance = (Math.random() * 1800);
+        this.size = (Math.random() * 50)
+
+
+    }
+    fire() {
+        let node1 = document.createElement("img")
+        node1.src = "/client/img/Explosion.png"
+        node1.setAttribute("id", "fire")
+        let parentNode = document.querySelector("#background")
+        parentNode.append(node1)
+        node1.style.top = this.height + "px"
+        node1.style.left = this.distance + "px"
+        node1.style.height = ((Math.random() * 70) + 40) + "px"
+        console.log(node1.style.top)
+
+    }
+}
+
+class Helicopter {
+
+    constructor(id) {
+        this.node = document.querySelector("#" + id)
+        this.height = 100;
+        this.distance = 1750;
+        this.speed = 1;
+    }
+
+    helicopter() {
+        let node = document.createElement("img")
+        node.src = "/client/img/helico2.gif"
+        node.setAttribute("id", "helico")
+        let parentNode = document.querySelector("#background")
+        parentNode.append(node)
+        node.style.top = this.height + "px"
+        node.style.left = this.distance + "px"
+
+    }
+
+    move() {
+        this.distance -= this.speed
+        let node = document.querySelector("#helico")
+        node.style.left = this.distance + "px"
+
+
+        if (this.distance == 0) {
+            this.distance = 1750;
+            this.height = (Math.random() * 150)
+            let node1 = document.querySelector("#helico")
+            node1.style.top = this.height + "px"
+
+        }
+
+    }
+
+
+}
+
+class Firetruck {
+
+
+    constructor(id){
+        this.node = document.querySelector("#" + id)
+        this.height = 820;
+        this.distance = 1750;
+        this.speed = 2;
+    }
+
+    fireTruck(){
+        let node = document.createElement("img")
+        node.src = "/client/img/fireTruck2.png"
+        node.setAttribute("id", "firetruck")
+        let parentNode = document.querySelector("#background")
+        parentNode.append(node)
+        node.style.top = this.height + "px"
+        node.style.left = this.distance + "px"
+    }
+
+    moveTruck(){
+        this.distance -= this.speed
+        let node = document.querySelector("#firetruck")
+        node.style.left = this.distance + "px"
+
+        if()
+        
+
+    }
+}
