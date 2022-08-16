be61c8d448686ddba0a7cad9eea727fdbe6f0412
ThomasPelletier <thomaspelletier@hotmail.ca>
2022-08-15 20:29:54 -0400

    added index css

---
+++ client/src/explosion.js
@@ -0,0 +1,12 @@
+class explosion{
+
+    constructor(id){
+    this.node = document.querySelector("#" + id)
+    this.x = Math.random()*700
+    this.y = Math.random()*700
+    
+
+    }
+
+
+}
--- client/src/page-index.js
+++ client/src/page-index.js
@@ -1,7 +1,32 @@
 import {signin} from './chat-api';
+import explosion from "./explosion.js"
+
+console.log("lol")
 
 window.addEventListener("load", () => {
     document.querySelector("form").onsubmit = function () {
         return signin(this);
     }
+
+   
+
 });
+// let Deadpool = document.getElementById("deadpool")
+// console.log(Deadpool)
+// Deadpool.addEventListener("click", () => {
+//     let node = document.createElement("div")
+//     let parentNode = document.querySelector("#background")
+//     node.setAttribute("id", "msg")
+//     node.innerText = "Welcome to Deadpool Chat"
+//     parentNode.append(node);
+//     })
+
+const fire = () => {
+    let node1 = document.createElement("div")
+    node1.id.add("fire")
+    let parentNode = document.querySelector("#background")
+    parentNode.append(node1)
+}
+
+
+
